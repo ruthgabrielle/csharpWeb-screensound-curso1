ac3e722a56e7368ccf9038cb811cc1110b15486e
Ruth Gomes <rgabrielle@uolinc.com>
2024-06-06 17:06:31 -0300

    chore: database connection

---
+++ ScreenSound/Banco/Connection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+namespace ScreenSound.Banco;
+
+internal class Connection
+{
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Encrypt=False;" +
+        "Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    public SqlConnection ObterConexao()
+    {
+        return new SqlConnection(connectionString);
+    }
+}
+

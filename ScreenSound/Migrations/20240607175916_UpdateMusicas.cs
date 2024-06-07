937ca94880bc0c4fe5f040cdba890f2a39f02b90
Ruth Gomes <rgabrielle@uolinc.com>
2024-06-07 15:24:21 -0300

    working with proxies

---
+++ ScreenSound/Migrations/20240607175916_UpdateMusicas.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMusicas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 1 WHERE Id = 1");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

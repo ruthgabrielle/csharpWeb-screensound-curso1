0877ad690e61988b86848e136d7cd1ebc65cc294
Ruth Gomes <rgabrielle@uolinc.com>
2024-06-15 17:55:39 -0300

    get and post

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,17 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/Artistas", () =>
 {
     var dal = new DAL<Artista>(new ScreenSoundContext());
+    return Results.Ok(dal.Listar());
+});
 
-    return dal.Listar();
+app.MapGet("/Artistas/{nome}", (string nome) =>
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+    if (artista is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(artista);
+});
+
+app.MapPost("/Artistas", ([FromBody]Artista artista) =>
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    dal.Adicionar(artista);
+    return Results.Ok();
 });
 
 app.Run();
--- ScreenSound.Shared.Modelos/Modelos/Artista.cs
+++ ScreenSound.Shared.Modelos/Modelos/Artista.cs
@@ -1,9 +1,12 @@
-﻿namespace ScreenSound.Modelos; 
+﻿namespace ScreenSound.Modelos;
 
-public class Artista 
+public class Artista
 {
     public virtual ICollection<Musica> Musicas { get; set; } = new List<Musica>();
-
+    public Artista()
+    {
+        FotoPerfil = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
+    }
     public Artista(string nome, string bio)
     {
         Nome = nome;
@@ -12,7 +15,7 @@ public class Artista
     }
 
     public string Nome { get; set; }
-    public string FotoPerfil { get; set; }
+    public string? FotoPerfil { get; set; }
     public string Bio { get; set; }
     public int Id { get; set; }
 
@@ -26,7 +29,7 @@ public class Artista
         Console.WriteLine($"Discografia do artista {Nome}");
         foreach (var musica in Musicas)
         {
-            Console.WriteLine($"Música: {musica.Nome} - Ano Lançamento: {musica.AnoLancamento}");
+            Console.WriteLine($"Música: {musica.Nome} - Ano de Lançamento: {musica.AnoLancamento}");
         }
     }
 
